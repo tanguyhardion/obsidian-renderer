826801996ff8cd379245dd92cd1bc19fbdf65bd9
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2024-03-31 23:36:50 +0200

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    basePath: '/obsidian-renderer',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
