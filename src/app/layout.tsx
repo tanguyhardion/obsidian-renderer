0d62e5a882e859807db894f711d09c6bbf03c02f
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2024-03-31 15:10:26 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.sass";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Obsidian Renderer",
+  description: "Web renderer of files in GitHub repositories used by Obsidian Git",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
