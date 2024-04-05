aafefbbb0ddba96233d756c425b4bc8fe4ec28b3
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2024-04-05 15:03:13 +0200

    custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="center">
+      <h1>In progress.</h1>
+      <div>
+        <Link href="/">Go back to home page</Link>
+      </div>
+    </div>
+  );
+}
